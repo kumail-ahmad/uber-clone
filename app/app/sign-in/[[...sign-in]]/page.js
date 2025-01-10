40c6f8d0c05610bad5290b1892ee809308c1b802
Kumail Ahmad <kumailahmad217@gmail.com>
2025-01-10 22:38:28 +0530

    testing

--- app/app/sign-in/[[...sign-in]]/page.js
+++ app/app/sign-in/[[...sign-in]]/page.js
@@ -1,11 +1,23 @@
-import { SignIn, useUser } from '@clerk/nextjs'
+import { SignIn, useUser } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Home() {
-  const { user } = useUser()
+  const { user } = useUser();
 
   if (!user) {
-    return <SignIn />
+    return (
+      <div>
+        <Image src="/banner.png" height={780} width={1500} alt=""></Image>
+
+        {/* <SignIn /> */}
+      </div>
+    );
   }
 
-  return <div>Welcome!</div>
+  return (
+    <div>
+      <h1>Welcome, {user.firstName}!</h1>
+      {/* Add your authenticated content here */}
+    </div>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Image src="/banner.png" height={780} width={1500} alt=""></Image>
+    </div>
+  );
 }
