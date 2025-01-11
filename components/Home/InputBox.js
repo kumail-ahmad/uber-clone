0b46c8fc346e6f81c9938386fcf35e1503e51d36
Kumail Ahmad <kumailahmad217@gmail.com>
2025-01-11 22:46:40 +0530

    added suggestions from api in dropbox

--- components/Home/InputBox.js
+++ components/Home/InputBox.js
@@ -7,21 +7,19 @@ const InputBox = ({ type }) => {
   const [value, setValue] = useState("");
   const [suggestions, setSuggestions] = useState([]);
 
-  const geoapifyApiKey = process.env.NEXT_PUBLIC_GEOAPIFY_API_KEY; 
+  const geoapifyApiKey = process.env.NEXT_PUBLIC_GEOAPIFY_API_KEY;
 
   const fetchSuggestions = async (query) => {
     if (!query) {
-      setSuggestions([]); 
+      setSuggestions([]);
       return;
     }
 
     try {
-     
       const response = await axios.get(
         `https://api.geoapify.com/v1/geocode/autocomplete?text=${query}&limit=5&apiKey=${geoapifyApiKey}`
       );
 
-      
       if (response.data.features) {
         setSuggestions(response.data.features);
       }
@@ -31,15 +29,18 @@ const InputBox = ({ type }) => {
   };
 
   useEffect(() => {
-    fetchSuggestions(value);  
+    fetchSuggestions(value);
   }, [value]);
 
   const handleSelect = (selectedAddress) => {
-    setValue(selectedAddress.properties.formatted);  
-    setSuggestions([]);  
-  };
-
-
+    setValue(selectedAddress.properties.formatted);
+    setSuggestions([]);
+  
+  const { lat, lon } = selectedAddress.properties;
+  console.log("Selected Address:", selectedAddress.properties.formatted);
+  console.log("Latitude:", lat);
+  console.log("Longitude:", lon);
+};
   return (
     <div className="relative p-2 bg-gray-100 rounded-2xl mt-3 flex items-center gap-4 ml-6">
       <Image src="/dot.svg" alt="dot" width={42} height={30} />
@@ -47,19 +48,19 @@ const InputBox = ({ type }) => {
       <input
         type="text"
         value={value}
-        onChange={(e) => setValue(e.target.value)}  
+        onChange={(e) => setValue(e.target.value)}
         placeholder={type === "source" ? "Pickup location" : "Drop location"}
         className="bg-transparent w-[270px] px-3 py-2 rounded-lg outline-black"
       />
 
-      {/* Suggestions dropdown */}
+    
       {suggestions.length > 0 && (
         <div className="autocomplete-suggestions absolute top-full left-0 mt-2 bg-white border border-gray-300 rounded-lg w-full z-10">
           {suggestions.map((suggestion) => (
             <div
               key={suggestion.properties.place_id}
               className="p-2 cursor-pointer hover:bg-gray-200"
-              onClick={() => handleSelect(suggestion)}  // Handle selection
+              onClick={() => handleSelect(suggestion)} // Handle selection
             >
               {suggestion.properties.formatted}
             </div>
