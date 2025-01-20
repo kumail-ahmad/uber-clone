0fffe1abaa270abb4a8ae21c48de0a1278717165
Kumail Ahmad <kumailahmad217@gmail.com>
2025-01-20 22:29:51 +0530

    Designed Car section

--- components/Home/CarListOption.js
+++ components/Home/CarListOption.js
@@ -6,9 +6,12 @@ const CarListOption = () => {
   return (
     <div>
       <p className="font-bold  mt-5 text-4xl">Choose a ride</p>
-      <h1 className="font-bold text-2xl mt-5">Recomended:</h1>
+      <h1 className="font-bold text-2xl mt-5">Recomended</h1>
       {carList.map((item) => (
-        <div key={item.id} className="p-4 border rounded-md mb-2 mt-3 flex gap-2  hover:bg-gray-200">
+        <div
+          key={item.id}
+          className="p-4 border rounded-md mb-2 mt-3 flex gap-2  hover:bg-gray-200"
+        >
           <Image
             src={item.image}
             alt={item.name}
@@ -17,11 +20,14 @@ const CarListOption = () => {
             className="mt-2 object-contain"
           />{" "}
           <div className="second">
-            <h3 className="text-lg font-bold">{item.name}</h3>
+            <p className="text-sm flex items-center   gap-1">
+              <h3 className="text-lg font-bold">{item.name}</h3>
+              <Image src="/person.svg" alt="" width={14} height={14} />{" "}
+              <div className="capacity">{item.capactiy}</div>
+            </p>
             <p className="text-sm">{item.desc}</p>
           </div>
-          <p className="text-sm">Capacity: {item.capactiy}</p>
-          <p className="price">${item.amount*100}</p>
+          <p className="price">${item.amount * 100}</p>
         </div>
       ))}
     </div>
