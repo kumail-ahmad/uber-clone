2c7c1c51830856f674bce905fe043060d383a2bb
Kumail Ahmad <kumailahmad217@gmail.com>
2025-01-20 22:41:41 +0530

    Added scrollbar

--- components/Home/CarListOption.js
+++ components/Home/CarListOption.js
@@ -4,13 +4,13 @@ import Image from "next/image";
 
 const CarListOption = () => {
   return (
-    <div>
+    <div className="h-[200px] overflow-auto w-[420px]">
       <p className="font-bold  mt-5 text-4xl">Choose a ride</p>
       <h1 className="font-bold text-2xl mt-5">Recomended</h1>
       {carList.map((item) => (
         <div
           key={item.id}
-          className="p-4 border rounded-md mb-2 mt-3 flex gap-2  hover:bg-gray-200"
+          className="p-4 border rounded-md mb-2 mt-3 flex gap-2  hover:bg-gray-200 overflow-auto w-[380px] "
         >
           <Image
             src={item.image}
@@ -20,14 +20,14 @@ const CarListOption = () => {
             className="mt-2 object-contain"
           />{" "}
           <div className="second">
-            <p className="text-sm flex items-center   gap-1">
-              <h3 className="text-lg font-bold">{item.name}</h3>
+            <div className="text-sm flex items-center   gap-1">
+              <p className="text-lg font-bold">{item.name}</p>
               <Image src="/person.svg" alt="" width={14} height={14} />{" "}
               <div className="capacity">{item.capactiy}</div>
-            </p>
+            </div>
             <p className="text-sm">{item.desc}</p>
           </div>
-          <p className="price">${item.amount * 100}</p>
+          <p className="price">₹{item.amount * 100}</p>
         </div>
       ))}
     </div>
