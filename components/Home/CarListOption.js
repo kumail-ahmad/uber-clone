a1eb62fe1e47449f2edc401d4022e01b7c8b0c35
Kumail Ahmad <kumailahmad217@gmail.com>
2025-01-20 21:46:13 +0530

    Modified Ui

--- components/Home/CarListOption.js
+++ components/Home/CarListOption.js
@@ -5,8 +5,10 @@ import Image from "next/image";
 const CarListOption = () => {
   return (
     <div>
+      <p className="font-bold  mt-5 text-4xl">Choose a ride</p>
+      <h1 className="font-bold  mt-5">Recomended:</h1>
       {carList.map((item) => (
-        <div key={item.id} className="p-4 border rounded-md mb-2">
+        <div key={item.id} className="p-4 border rounded-md mb-2 mt-3 ">
           <h3 className="text-lg font-bold">{item.name}</h3>
           <p className="text-sm">{item.desc}</p>
           <p className="text-sm">Capacity: {item.capactiy}</p>
